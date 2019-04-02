1beae06587a1bf27512d31dd53a340c8b9a37ba8
NHaider <NHaider@NUC_i3>
2019-04-02 17:11:02 +0200

    Default video length 20s

--- php/config.php
+++ php/config.php
@@ -6,7 +6,7 @@ $config_dayendHour = 21;
 // ignore days of the week numeric ISO-8601 1 Monday 7 Sunday 
 //$config_ignoreDays = [6,7]; //ignore Saturday and Sunday
 $config_videoDir ="/tilacam/video";
-$config_videoMaxLength = 30; //in seconds this results in limit the Pictures 
+$config_videoMaxLength = 20; //in seconds this results in limit the Pictures 
 //image pattern (php date format not complemtly implemented) $y=year 2 digits, %Y=year 4 digits, m=month 2 digits, d=day 2 digits,H=hour 2 digits,i=minute 2 digits
 //all not numeric char are removed
 $config_pictureNamePattern = '%d%m%Y%H%i';
